ab65848c508eac3f9ceb0afd3556482741284eb0
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-07-20 20:46:59 +0600

    splash screen design done

--- lib/screens/onBoarding/splash_screen.dart
+++ lib/screens/onBoarding/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:task_manager/style/style.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,10 +12,20 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Splash Screen"),
-      ),
-    );
+    return Scaffold(
+        body: Stack(
+      children: [
+        screenBackground(context),
+        Container(
+          padding: const EdgeInsets.all(30),
+          child: Center(
+            child: SvgPicture.asset(
+              "assets/images/logo.svg",
+              alignment: Alignment.center,
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
