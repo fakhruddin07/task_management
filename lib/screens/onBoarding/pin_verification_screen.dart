724777e935d63dd97b84b45c2b8d11a5fdb1893e
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-07-23 17:12:57 +0600

    Pin Verification

--- lib/screens/onBoarding/pin_verification_screen.dart
+++ lib/screens/onBoarding/pin_verification_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:task_manager/api/apiClient.dart';
+import 'package:task_manager/utility/utility.dart';
 
 import '../../style/style.dart';
 
@@ -11,6 +13,40 @@ class PinVerificationScreen extends StatefulWidget {
 }
 
 class _PinVerificationScreen extends State<PinVerificationScreen> {
+  Map<String, String> formValues = {"otp": ""};
+  bool isLoading = false;
+
+  inputOnChange(mapKey, textValue) {
+    if (mounted) {
+      setState(() {
+        formValues.update(mapKey, (value) => textValue);
+      });
+    }
+  }
+
+  formOnSubmit() async {
+    if (formValues["otp"]!.trim().isEmpty) {
+      errorToast("OTP Required");
+    } else {
+      if (mounted) {
+        setState(() {
+          isLoading = true;
+        });
+      }
+      String? email = await readUserData("email");
+      bool response = await otpVerifyRequest(email, formValues["otp"]);
+      if (response == true) {
+        Navigator.pushNamed(context, "/setPassword");
+      } else {
+        if (mounted) {
+          setState(() {
+            isLoading = true;
+          });
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,41 +54,52 @@ class _PinVerificationScreen extends State<PinVerificationScreen> {
         children: [
           screenBackground(context),
           Container(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Pin Verification",
-                  style: head1Text(colorDarkBlue),
-                ),
-                const SizedBox(height: 1),
-                Text(
-                  "A 6 digit verification pin will send to your email address",
-                  style: head6Text(colorLightGray),
-                ),
-                const SizedBox(height: 20),
-                PinCodeTextField(
-                  appContext: context,
-                  length: 6,
-                  pinTheme: appOtpStyle(),
-                  animationType: AnimationType.fade,
-                  animationDuration: const Duration(milliseconds: 300),
-                  enableActiveFill: true,
-                  onCompleted: (v){},
-                  onChanged: (value){},
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: appButtonStyle(),
-                    child: successButtonChild("Verify"),
+            alignment: Alignment.center,
+            child: isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : SingleChildScrollView(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Pin Verification",
+                          style: head1Text(colorDarkBlue),
+                        ),
+                        const SizedBox(height: 1),
+                        Text(
+                          "A 6 digit verification pin will send to your email address",
+                          style: head6Text(colorLightGray),
+                        ),
+                        const SizedBox(height: 20),
+                        PinCodeTextField(
+                          appContext: context,
+                          length: 6,
+                          pinTheme: appOtpStyle(),
+                          animationType: AnimationType.fade,
+                          animationDuration: const Duration(milliseconds: 300),
+                          enableActiveFill: true,
+                          onCompleted: (v) {},
+                          onChanged: (textValue) {
+                            inputOnChange("otp", textValue);
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              formOnSubmit();
+                            },
+                            style: appButtonStyle(),
+                            child: successButtonChild("Verify"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
           ),
         ],
       ),
